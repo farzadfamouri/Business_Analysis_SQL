d556f30852e4f9795c292065f6c24ab9351c6c6a
farzadfamouri <95972923+farzadfamouri@users.noreply.github.com>
2024-12-04 22:37:39 -0500

    Create Income from movies.sql

---
+++ Data-Driven Decision Making in SQL/Income from movies.sql
@@ -0,0 +1,10 @@
+SELECT rm.title, -- Report the income from movie rentals for each movie
+       SUM(rm.renting_price) AS income_movie
+FROM
+       (SELECT m.title,  
+               m.renting_price
+       FROM renting AS r
+       LEFT JOIN movies AS m
+       ON r.movie_id=m.movie_id) AS rm
+GROUP BY rm.title
+ORDER BY income_movie DESC; -- Order the result by decreasing income

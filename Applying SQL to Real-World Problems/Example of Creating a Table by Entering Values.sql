b12dd835df006c5f3c56b6236fae1b2b974385bb
farzadfamouri <95972923+farzadfamouri@users.noreply.github.com>
2024-12-09 20:41:08 -0500

    Create Example of Creating a Table by Entering Values.sql

---
+++ Applying SQL to Real-World Problems/Example of Creating a Table by Entering Values.sql
@@ -0,0 +1,17 @@
+-- Create a new table called oscars
+CREATE TABLE oscars (
+    title VARCHAR,
+    award VARCHAR
+);
+
+-- Insert the data into the oscars table
+INSERT INTO oscars (title, award)
+VALUES
+('TRANSLATION SUMMER', 'Best Film'),
+('DORADO NOTTING', 'Best Film'),
+('MARS ROMAN', 'Best Film'),
+('CUPBOARD SINNERS', 'Best Film'),
+('LONELY ELEPHANT', 'Best Film');
+
+SELECT * 
+FROM oscars;

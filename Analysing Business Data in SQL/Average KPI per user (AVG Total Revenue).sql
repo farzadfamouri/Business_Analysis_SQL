76053a1a32e459c8091ff46e69c5520a73eb3cca
farzadfamouri <95972923+farzadfamouri@users.noreply.github.com>
2024-12-14 17:14:53 -0500

    Create Average KPI per user (AVG Total Revenue).sql

---
+++ Analysing Business Data in SQL/Average KPI per user (AVG Total Revenue).sql
@@ -0,0 +1,12 @@
+-- Create a CTE named kpi
+WITH kpi AS (
+  SELECT
+    -- Select the user ID and calculate revenue
+    user_id,
+    SUM(m.meal_price * o.order_quantity) AS revenue
+  FROM meals AS m
+  JOIN orders AS o ON m.meal_id = o.meal_id
+  GROUP BY user_id)
+-- Calculate ARPU
+SELECT ROUND(AVG(revenue) :: NUMERIC , 2) AS arpu
+FROM kpi;

7332aa3e617a6a2ef2ce49f2db6af94d74efea00
farzadfamouri <95972923+farzadfamouri@users.noreply.github.com>
2024-12-14 17:16:10 -0500

    Create user's revenue by month before pivoting.sql

---
+++ Analysing Business Data in SQL/user's revenue by month before pivoting.sql
@@ -0,0 +1,10 @@
+SELECT
+    user_id,
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    SUM(meal_price * order_quantity) :: FLOAT AS revenue
+  FROM meals
+  JOIN orders ON meals.meal_id = orders.meal_id
+ WHERE user_id IN (0, 1, 2, 3, 4)
+   AND order_date < '2018-09-01'
+ GROUP BY user_id, delivr_month
+ ORDER BY user_id, delivr_month;

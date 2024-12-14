db5469ba8f2c467de2af93b6d70766e3e9985d74
farzadfamouri <95972923+farzadfamouri@users.noreply.github.com>
2024-12-14 17:15:29 -0500

    Create Count Interquartile data.sql

---
+++ Analysing Business Data in SQL/Count Interquartile data.sql
@@ -0,0 +1,28 @@
+WITH user_revenues AS (
+  SELECT
+    -- Select user_id and calculate revenue by user 
+    user_id,
+    SUM(m.meal_price * o.order_quantity) AS revenue
+  FROM meals AS m
+  JOIN orders AS o ON m.meal_id = o.meal_id
+  GROUP BY user_id),
+
+  quartiles AS (
+  SELECT
+    -- Calculate the first and third revenue quartiles
+    ROUND(
+      PERCENTILE_CONT(0.25) WITHIN GROUP
+      (ORDER BY revenue ASC) :: NUMERIC,
+    2) AS revenue_p25,
+    ROUND(
+      PERCENTILE_CONT(0.75) WITHIN GROUP
+      (ORDER BY revenue ASC) :: NUMERIC,
+    2) AS revenue_p75
+  FROM user_revenues)
+
+SELECT 
+	COUNT(user_id) AS users
+FROM user_revenues
+CROSS JOIN quartiles
+WHERE revenue :: NUMERIC >= revenue_p25
+  AND revenue :: NUMERIC <= revenue_p75;

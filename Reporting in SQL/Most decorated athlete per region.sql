d1773dc2909e664e865e0b1ee8f2bdc70be98127
farzadfamouri <95972923+farzadfamouri@users.noreply.github.com>
2024-12-18 19:38:19 -0500

    Create Most decorated athlete per region.sql

---
+++ Reporting in SQL/Most decorated athlete per region.sql
@@ -0,0 +1,22 @@
+-- Query region, athlete name, and total_golds
+SELECT 
+	region,
+    athlete_name,
+    total_golds
+FROM
+    (SELECT 
+		-- Query region, athlete_name, and total gold medals
+        region, 
+        name AS athlete_name, 
+        SUM(gold) AS total_golds,
+        -- Assign a regional rank to each athlete
+        ROW_NUMBER() OVER (PARTITION BY region ORDER BY SUM(gold) DESC) AS row_num
+    FROM summer_games_clean AS s
+    JOIN athletes AS a
+    ON a.id = s.athlete_id
+    JOIN countries AS c
+    ON s.country_id = c.id
+    -- Alias as subquery
+    GROUP BY region, athlete_name) AS subquery
+-- Filter for only the top athlete per region
+WHERE row_num =1;
